e59ef3c4497f5b76b579e8f62fae728c9294d490
Alan Hemmings <goblinfactory@gmail.com>
2018-04-05 22:19:22 +0100

    temporarily adding chromedriver, should not be included in source, will remove later

---
+++ update-chrome-driver.ps1
@@ -0,0 +1,17 @@
+﻿# find the latest chromedriver version by scraping the link on the chromedriver webpage
+# get the latest chromedriver version number from the end of the url 
+# e.g. 2.37 from -> https://chromedriver.storage.googleapis.com/index.html?path=2.37/
+# ------------------------------------------------------------------------------------
+
+$res = Invoke-WebRequest "https://sites.google.com/a/chromium.org/chromedriver/downloads"
+$url = ($res.Links | Where-Object -property href -match "^https://chromedriver.storage.googleapis.com/index.html\?path=" | select -property href)[0].href
+
+$seperator = @("?path=")
+$ver = $url.Split($seperator, [System.StringSplitOptions]::RemoveEmptyEntries)[1]
+
+$file = "https://chromedriver.storage.googleapis.com/" + $ver + "chromedriver_win32.zip"
+write-host "downloading $file"
+Invoke-WebRequest $file -OutFile "FluentAutomation.SeleniumWebDriver\3rdPartyLib\chromedriver.exe"
+
+
+                  
